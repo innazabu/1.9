24d6bb48c82c0b037735f4b26194b1180c36c84f
innazabu <inna.zabu09@gmail.com>
2024-06-23 19:10:55 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,25 @@
+a='roza'
+print(len(a)) #длина слова
+
+
+first=5
+second=8
+summa=first+second
+diff=first-second
+print(summa) #сумма
+print(diff) #разность
+
+
+third=45
+mean=(first+second+third)/3
+print(mean)
+
+first_string='Вторник'
+second_string='Понедельник'
+print(second_string+', '+first_string)
+
+a=13
+b=23
+c=5
+f=(a * b) + (a * c)
+print((f**3)/2)
